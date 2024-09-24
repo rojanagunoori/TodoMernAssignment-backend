d77c6f36ac0d39dd2bc9c3542fd9a2b6c3a41177
rojanagunooridgtlmart <rojadgtlmart@gmail.com>
2024-09-25 01:36:45 +0530

    create task error

--- server.js
+++ server.js
@@ -190,10 +190,10 @@ app.delete("/deleteprofile", authenticateJWT, (req, res) => {
     });
 });
 
-app.use((err, req, res, next) => {
+/*app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
-});
+});*/
 
 
 app.listen(PORT,()=>{
